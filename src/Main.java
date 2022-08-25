81ad98f69062d2cefe33290c8ea30c15acf5c76e
jah <celt8@yandex.ru>
2022-08-25 12:15:43 +0500

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+//Exercise 1
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+        for (i = 10; i>=1; i--){
+            System.out.print(i+" ");
+        }
 
+        //Exercise 2
+        System.out.println();
+        int friday = 1;
+        for(int j=friday; j<=31; j=j+7){
+            System.out.println("Сегодня пятница, "+j+"-е число. Необходимо подготовить отчет.");
+
+        }
+
+        //Exersice 3
+        int currentYear = 2017;
+        int yearBefor = currentYear - 200;
+        int yearAfter = currentYear + 100;
+        for(int g = yearBefor; g<yearAfter; ){
+            g = g+79;
+            System.out.println(g);
+        }
 
     }
 }
